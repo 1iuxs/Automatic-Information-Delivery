c3a2ac736a3d36ae1bcf184835e151f8202be1eb
1iuxs <147172548+1iuxs@users.noreply.github.com>
2026-06-26 20:51:07 +0800

    Update fasttext.py

--- fasttext.py
+++ fasttext.py
@@ -1,46 +1,27 @@
 
 
-import time
-import jieba
 import fasttext
+import time
 
-from flask import Flask
-from flask import request
-
-
-app = Flask(__name__)
-# jieba.load_userdict("stopwords.txt")
-
-stop_words = set()
-# 替换成你的stopwords.txt相对路径
-with open('stopwords.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        word = line.strip()
-        if word:  # 跳过空行
-            stop_words.add(word)
-
-model_save_path = "model_fast.bin_2.0"
-model = fasttext.load_model(model_save_path)
-
-print("模型加载完成")
+train_data_path = 'train.txt'
+test_data_path = 'test.txt'
+dev_data_path = 'dev.txt'
+# 开启模型训练
+model = fasttext.train_supervised(
+    input=train_data_path,
+    autotuneValidationFile=dev_data_path,
+    autotuneDuration=300,
+    wordNgrams=2,
+    verbose=3)
+#supervised 监督
+#unsupervised 无监督
+# 开启模型测试
+result = model.test(test_data_path)
+print(result)
 
 
-
-@app.route('/v1/main_server', methods=['POST'])
-def main_server():
-    uid = request.form['uid']
-    text = request.form['text']
-    input_text = ' '.join(jieba.cut(text))
-    res = model.predict(input_text)
-    predict_name = res[0][0]
-    return predict_name     
-
-
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000)
-
-
-
-
-
+#模型保存
+time1 = int(time.time())
+model_save_path = 'model_fast.bin_2.0'
+model.save_model(model_save_path)
 
